8531c138f13461844ef8fc809be9f98fc4e7e1aa
edwiersma <edwiersma@gmail.com>
2026-05-08 15:50:01 +0200

    Fixed Fatal Error, fixed answer validating, Added stat attribute, Added basic character backgrounds

--- dcrawl/dcrawl.py
+++ dcrawl/dcrawl.py
@@ -6,8 +6,12 @@ DEBUG = sys.platform != 'emscripten'
 if DEBUG:
     from cmd_interface import CommandHandler
     from engine import GameInit
-    with open('dcrawl.json') as json_file:
-        GAME_DATA = json.load(json_file)
+    try:
+        with open('dcrawl.json') as json_file:
+            GAME_DATA = json.load(json_file)
+    except:
+        with open('dcrawl/dcrawl.json') as json_file:
+            GAME_DATA = json.load(json_file)
 
 
 def get_game_data(search_set, key, value):
@@ -19,23 +23,27 @@ class GameInitIns(GameInit):
         super().__init__()
         class_dict = get_game_data("object_definition", "object_class", "C_PlayerClass")
         race_dict = get_game_data("object_definition", "object_class", "C_PlayerRace")
-        # background_dict = get_game_data("object_definition", "object_class", "PlayerRace")
+        background_dict = get_game_data("object_definition", "object_class", "C_PlayerBackground")
         self.character_classes = (
             [c.get("class_weapon").lower() for c in class_dict.values()],
             " / ".join([f"<{c.get('color')}>{c.get('class_weapon')}</{c.get('color')}>" for c in class_dict.values()])
         )
         self.character_races = (
-            [k for k in race_dict.keys()],
-            " / ".join([f"<o>{v.get("name")}</o>" for v in race_dict.values()])
+            [v.get("name").lower() for v in race_dict.values()],
+            " / ".join([f"<o>{v}</o>" for v in race_dict.keys()])
+        )
+        self.character_backgrounds = (
+            [v.get("name").lower() for v in background_dict.values()],
+            " / ".join([f"<o>{v}</o>" for v in background_dict.keys()])
         )
 
     def fnc_set_player_class(self, cmd, arg):
-        print(f"### {cmd}: {arg}")
         matched = [
             c for c in get_game_data("object_definition", "object_class", "C_PlayerClass").values()
             if cmd.lower() in c["class_weapon"].lower()
         ]
-        self.player.player_class = self._create_instance(obj_name=matched[0].get("name").lower(), struct=matched[0])
+        class_instance = self._create_instance(obj_name=f"{arg}{matched[0].get('name').lower()}", struct=matched[0])
+        self.player.player_class = class_instance
 
 
 handler = CommandHandler()
@@ -55,12 +63,11 @@ def send_cmd(cmd: str) -> str:
 
 if DEBUG:
     print(intro())
-    print(send_cmd("y"))
     print(send_cmd("1"))
     print(send_cmd("Bob"))
     print(send_cmd("half-elf"))
     print(send_cmd("Sword"))
     print(send_cmd("Criminal"))
     print(send_cmd("y"))
-    print(send_cmd("pm"))
     print(send_cmd("5"))
+    print(send_cmd("pm"))
